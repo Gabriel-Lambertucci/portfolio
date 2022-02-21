0ea66c09771c6894cc26da27b476cee6ab041ca5
Gabriel Lambertucci <gabriellambertucci10@gmail.com>
2022-02-21 19:04:25 -0300

    Atualizando

--- portfolio/src/components/ProjectPagesHeader.jsx
+++ portfolio/src/components/ProjectPagesHeader.jsx
@@ -6,7 +6,9 @@ class ProjectPagesHeader extends Component {
   render() {
     return( 
       <header className="header">
-        <video id='video' src="https://cdn-icons-mp4.flaticon.com/512/6569/6569144.mp4" autoPlay='true' loop="true" />
+        <div className="video-div">
+          <video id='video' src="https://cdn-icons-mp4.flaticon.com/512/6569/6569144.mp4" autoPlay='true' loop="true" />
+        </div>
         <nav className="nav">
           <ul className="unordered-list">
             <li class='item-list'> <Link id='Link' to='/'> Home </Link> </li>
