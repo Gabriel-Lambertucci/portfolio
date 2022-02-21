9c55a193b6b4bdeec49d75816148df1d9598858a
Gabriel Lambertucci <gabriellambertucci10@gmail.com>
2022-02-21 18:27:59 -0300

    Atualizando

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import '../style/Skills.css'
 
+const reactImg = require('../images/React.png');
+
 class Skills extends Component {
   state = {
     phrases: ['Passe o mouse pelas Logos para informações', 'O React é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web. É utilizado nos sites da Netflix, Imgur, Feedly, Airbnb, SeatGeek, HelloSign, Walmart e outros.', 'CSS3 é a terceira mais nova versão das famosas Cascading Style Sheets, pela qual se define estilos para um projeto web. Com efeitos de transição, imagem, imagem de fundo/background e outros, pode-se criar estilos únicos para seus projetos web, alterando diversos aspectos de design no layout da página.', 'HTML5 é uma linguagem de marcação para a World Wide Web e é uma tecnologia chave da Internet, originalmente proposto por Opera Software. É a quinta versão da linguagem HTML.', 'JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web.'],
@@ -32,7 +34,7 @@ class Skills extends Component {
       <section className='Skills-section'>
         <div className = 'logos'>
           <h2 id='h2-skills'>Skills</h2>
-          <img onMouseOver={this.mouseOverHandler} onMouseOut={this.mouseOutHandler} className='icons' src='https://cdn-icons.flaticon.com/png/512/1183/premium/1183672.png?token=exp=1645382528~hmac=03ed3a82edb09f50ce5aafc08366ecbf' alt='React logo'></img>
+          <img onMouseOver={this.mouseOverHandler} onMouseOut={this.mouseOutHandler} className='icons' src={ reactImg } alt='React logo'></img>
           <img onMouseOver={this.mouseOverHandler} onMouseOut={this.mouseOutHandler} className='icons' src='https://cdn-icons-png.flaticon.com/512/888/888847.png' alt='Css logo'></img>
           <img onMouseOver={this.mouseOverHandler} onMouseOut={this.mouseOutHandler} className='icons' src='https://cdn-icons-png.flaticon.com/512/888/888859.png' alt='Html logo'></img>
           <img onMouseOver={this.mouseOverHandler} onMouseOut={this.mouseOutHandler} className='icons' id='js-logo' src='https://cdn-icons-png.flaticon.com/512/5968/5968292.png' alt='JS logo'></img>
