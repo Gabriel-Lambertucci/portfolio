81e851777b33b94a6cbd8325175fa60a0fb2a6fc
Gabriel Lambertucci <gabriellambertucci10@gmail.com>
2022-03-18 15:16:46 -0300

    Atualizando Portfolio

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import '../style/Projects.css';
 
 let gifTrybeTunes = require('../videos/TrybeTunes.gif');
+let gifTrivia = require('../videos/Trivia.mp4');
 
-const trybeTunesUrl = 'https://gabriel-lambertucci.github.io/TrybeTunes/'
+const trybeTunesUrl = 'https://gabriel-lambertucci.github.io/TrybeTunes/';
+const triviaURL = 'https://gabriel-lambertucci.github.io/Trivia-Game/';
 
 class Projects extends Component {
   render() {
@@ -11,6 +13,7 @@ class Projects extends Component {
       <section className='projects'> 
           <h2 id='projetos-h2'>Projetos</h2>
         <div className='projects-section'>
+          <div className='project-trybetunes'>
             <a className='gif-link'  href={ trybeTunesUrl } target='_blank' rel='noreferrer'>
               <img className='project-img' src={gifTrybeTunes} alt='TrybeTunes' />
             </a>   
@@ -18,7 +21,18 @@ class Projects extends Component {
             <a className='gif-link'  href={ trybeTunesUrl } target='_blank' rel='noreferrer'>
               TrybeTunes
             </a>
-          </h3>
+            </h3>
+          </div>
+          <div className='project-trivia'>
+              <a className='gif-link'  href={ triviaURL } target='_blank' rel='noreferrer'>
+                <video className='project-img' src={gifTrivia} autoplay='true' loop='true' muted />
+              </a>   
+              <h3>
+              <a className='gif-link'  href={ triviaURL } target='_blank' rel='noreferrer'>
+                Jogo de Trivia
+              </a>
+            </h3>
+          </div>
         </div>
       </section>
     )
